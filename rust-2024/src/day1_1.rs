1f389ec028e17cebe22f50b8244ebe8bc767388a
jomaro <jomaro.rodrigues@gmail.com>
2024-12-02 09:27:59 -0300

    rust-2024: day 1

---
+++ rust-2024/src/day1_1.rs
@@ -0,0 +1,31 @@
+
+use std::io::{self, BufRead};
+
+
+pub fn solve() -> () {
+    let (mut left, mut right): (Vec<_>, Vec<_>) = io::stdin().lock().lines()
+        .map(|line| {
+          let numbers : Vec<i64> =
+            line
+              .unwrap()
+              .split("   ")
+              .map(|number| number.parse::<i64>().unwrap())
+              .collect();
+
+          (numbers[0], numbers[1])
+        })
+        .unzip();
+
+    left.sort_unstable();
+    right.sort_unstable();
+
+    let value : u64 = left.into_iter().zip(right).map(|(l, r)| (l - r).abs() as u64).sum();
+
+    println!("{}", value);
+
+    ()
+}
+
+
+// 30047353
+// 30047353
---
+++ rust-2024/src/day1_2.rs
@@ -0,0 +1,36 @@
+
+use std::io::{self, BufRead};
+use std::collections::HashMap;
+
+
+pub fn solve() -> () {
+    let (left, right): (Vec<_>, Vec<_>) = io::stdin().lock().lines()
+        .map(|line| {
+          let numbers : Vec<u64> =
+            line
+              .unwrap()
+              .split("   ")
+              .map(|number| number.parse::<u64>().unwrap())
+              .collect();
+
+          (numbers[0], numbers[1])
+        })
+        .unzip();
+
+    let right_frequencies = frequency(&right);
+
+    let value : u64 = left.into_iter().map(|l| l * *right_frequencies.get(&l).unwrap_or(&0u64)).sum();
+
+    println!("{}", value);
+
+    ()
+}
+
+fn frequency(v: &Vec<u64>) -> HashMap<u64, u64> {
+  let mut m: HashMap<u64, u64> = HashMap::new();
+    for x in v {
+      *m.entry(*x).or_default() += 1;
+    }
+
+  m
+}
