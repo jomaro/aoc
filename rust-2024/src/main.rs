6202b501d56109e98ee2ad6a31fd71c8f51ce31e
jomaro <jomaro.rodrigues@gmail.com>
2024-12-03 10:07:36 -0300

    rust-2024: day 2

--- rust-2024/src/main.rs
+++ rust-2024/src/main.rs
@@ -3,18 +3,19 @@ use std::env;
 
 mod day1_1;
 mod day1_2;
-// mod day2_1;
-// mod day2_2;
+mod day2_1;
+mod day2_2;
 
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+  let args: Vec<String> = env::args().collect();
 
-    match args[1].as_str() {
-        "aoc1-1" => day1_1::solve(),
-        "aoc1-2" => day1_2::solve(),
-        // "aoc2-1" => day2_1::solve(),
-        // "aoc2-2" => day2_2::solve(),
-        _ => panic!("Parameter invalid!!"),
-    }
+  match args[1].as_str() {
+    "aoc1-1" => day1_1::solve(),
+    "aoc1-2" => day1_2::solve(),
+    "aoc2-1" => day2_1::solve(),
+    "aoc2-2" => day2_2::solve(),
+
+    _ => panic!("Parameter invalid!!"),
+  }
 }
