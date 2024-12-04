16e5e30de07783bb1bd692f29f9bbd0602ad6d1f
jomaro <jomaro.rodrigues@gmail.com>
2024-12-03 21:52:11 -0300

    rust-2024: day 3

--- rust-2024/src/main.rs
+++ rust-2024/src/main.rs
@@ -5,6 +5,8 @@ mod day1_1;
 mod day1_2;
 mod day2_1;
 mod day2_2;
+mod day3_1;
+mod day3_2;
 
 
 fn main() {
@@ -15,6 +17,8 @@ fn main() {
     "aoc1-2" => day1_2::solve(),
     "aoc2-1" => day2_1::solve(),
     "aoc2-2" => day2_2::solve(),
+    "aoc3-1" => day3_1::solve(),
+    "aoc3-2" => day3_2::solve(),
 
     _ => panic!("Parameter invalid!!"),
   }
